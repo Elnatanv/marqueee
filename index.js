9e71c8075b9ff1c73893e8a9420a1355b9dee703
Elnatanv <elnatanv@gmail.com>
2024-02-27 17:50:06 +0100

    test

--- index.js
+++ index.js
@@ -21,29 +21,30 @@
   </div>
  */
 export const initMarqueeeSlider = (id, options = {}) => {
-  const { stopOnHover = false } = options;
   const swiper = document.getElementById(id);
   const swiperWrapperInit = swiper.querySelector(".marquee-slider-wrapper");
   const swiperSlidesWrapper = swiperWrapperInit.getElementsByClassName(
     "marquee-slider-slides-wrapper"
   );
+
+  const { stopOnHover = false } = options;
+
+  const speed = parseFloat(swiper.getAttribute("data-speed"));
+
   const space = parseFloat(swiper.getAttribute("data-space")) || 10;
   const prefix = swiper.getAttribute("data-prefix") || "px";
+  const spaceBetween = { value: space, prefix: prefix };
 
   const initStyle = () => {
     swiper.style.overflow = "hidden";
-    //animate
-    const spaceBetween = { value: space, prefix: prefix };
     swiperSlidesWrapper[0].style.gap = `0 ${spaceBetween.value}${spaceBetween.prefix}`;
     swiperSlidesWrapper[0].style.margin = `0 ${spaceBetween.value / 2}${
       spaceBetween.prefix
     }`;
   };
-  initStyle();
+
   const clonedWrapper = swiperWrapperInit.cloneNode(true);
   const init = (swiperWrapper) => {
-    const speed = parseFloat(swiper.getAttribute("data-speed"));
-
     const width = swiperSlidesWrapper[0].offsetWidth;
 
     // Define keyframes for the animation
@@ -66,28 +67,31 @@ export const initMarqueeeSlider = (id, options = {}) => {
     }
 
     let swipersLength = swiperSlidesWrapper.length;
-    // duplicate
+
     for (let i = 0; i <= delta + 1; i++) {
       for (let j = 0; j < swipersLength; j++) {
         const elm = swiperSlidesWrapper[j];
 
-        const clone = elm.cloneNode(true); // Create a clone of the slide element
+        const clone = elm.cloneNode(true);
 
         swiperWrapper.appendChild(clone);
       }
     }
 
-    // Animate the movement to the left
-    const animationDuration = speed; // Adjust this value to control the animation speed
-
     // Apply CSS animation to the swiper wrapper
-    swiperWrapper.style.animation = `${id} ${animationDuration}s linear infinite`;
+    swiperWrapper.style.animation = `${id} ${speed}s linear infinite`;
 
     // Inject keyframes into a style element
     const styleElement = document.createElement("style");
     styleElement.innerHTML = keyframes;
     document.head.appendChild(styleElement);
 
+    // Reset the position when animation completes
+    swiperWrapper.addEventListener("animationiteration", () => {
+      swiperWrapper.style.transform = "translateX(0)";
+    });
+
+    //options handling
     if (stopOnHover) {
       swiperWrapper.addEventListener("mouseleave", () => {
         swiperWrapper.style.animationPlayState = "running";
@@ -97,12 +101,9 @@ export const initMarqueeeSlider = (id, options = {}) => {
         swiperWrapper.style.animationPlayState = "paused";
       });
     }
-    // Reset the position when animation completes
-    swiperWrapper.addEventListener("animationiteration", () => {
-      swiperWrapper.style.transform = "translateX(0)";
-    });
   };
 
+  initStyle();
   init(swiperWrapperInit);
 
   window.addEventListener("resize", init(clonedWrapper));
